785e1b52fe6133fdf42a856f9585309eb2130428
Ethan Lee <ethanl@tbd.email>
2024-01-11 14:39:45 -0800

    use `GestureDetector` and `AnimatedDefaultTextStyle` to animate font size

--- frontend/lib/shared/number_pad.dart
+++ frontend/lib/shared/number_pad.dart
@@ -11,9 +11,10 @@ class NumberPad extends HookWidget {
     final pressedKey = useState<String>('');
 
     return Container(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           _buildRow(context, ['1', '2', '3'], pressedKey),
           _buildRow(context, ['4', '5', '6'], pressedKey),
@@ -30,50 +31,55 @@ class NumberPad extends HookWidget {
     ValueNotifier<String> pressedKey,
   ) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: keys.map((key) {
-        return _buildKeyButton(context, key, pressedKey);
+        return NumberPadKey(title: key, onKeyPressed: onKeyPressed);
       }).toList(),
     );
   }
+}
 
-  Widget _buildKeyButton(
-    BuildContext context,
-    String key,
-    ValueNotifier<String> pressedKey,
-  ) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: Center(
-          child: FilledButton(
-            onPressed: () {
-              _handleKeyPress(pressedKey, key);
-            },
-            style: FilledButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.background,
+class NumberPadKey extends HookWidget {
+  final String title;
+  final Function(String) onKeyPressed;
+
+  const NumberPadKey(
+      {required this.title, required this.onKeyPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const defaultSize = 24.0;
+    const selectedSize = 40.0;
+
+    final keySize = useState(defaultSize);
+
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      width: 100.0,
+      height: 50.0,
+      child: GestureDetector(
+        onTapDown: (_) => keySize.value = selectedSize,
+        onTapCancel: () => keySize.value = defaultSize,
+        onTapUp: (_) {
+          keySize.value = defaultSize;
+          onKeyPressed(title);
+        },
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(milliseconds: 100),
+          style: TextStyle(
+            fontSize: keySize.value,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+          child: Center(
+            child: Text(
+              title,
+              textScaler: TextScaler.noScaling,
             ),
-            child: key.isNotEmpty
-                ? Text(
-                    key,
-                    style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: pressedKey.value == key ? 34.0 : 20.0,
-                        ),
-                  )
-                : const Spacer(),
           ),
         ),
       ),
     );
-  }
-
-  void _handleKeyPress(ValueNotifier<String> pressedKey, String key) {
-    pressedKey.value = key;
-    onKeyPressed(key);
-
-    Future.delayed(const Duration(milliseconds: 100), () {
-      pressedKey.value = '';
-    });
   }
 }
