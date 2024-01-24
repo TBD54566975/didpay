24d882230d2e777b91f626ca94a5516130ebd635
Ethan Lee <ethanl@tbd.email>
2024-01-23 18:34:59 -0800

    update tests for new send tab

--- frontend/test/features/app/app_tabs_test.dart
+++ frontend/test/features/app/app_tabs_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_starter/features/account/account_page.dart';
 import 'package:flutter_starter/features/app/app_tabs.dart';
 import 'package:flutter_starter/features/home/home_page.dart';
+import 'package:flutter_starter/features/send/send_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../helpers/widget_helpers.dart';
@@ -12,6 +13,16 @@ void main() {
     );
 
     expect(find.byType(HomePage), findsOneWidget);
+  });
+
+  testWidgets('should show SendPage when tapped', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      WidgetHelpers.testableWidget(child: const AppTabs()),
+    );
+
+    await tester.tap(find.text('Send'));
+    await tester.pumpAndSettle();
+    expect(find.byType(SendPage), findsOneWidget);
   });
 
   testWidgets('should show AccountPage when tapped',
