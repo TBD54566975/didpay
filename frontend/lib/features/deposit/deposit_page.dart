6988a528816fae26ab380d7e050cf051b023d5f5
Kirah Sapong <kirah@tbd.email>
2024-01-30 14:59:50 -0800

    update deposit page with shared components

--- frontend/lib/features/deposit/deposit_page.dart
+++ frontend/lib/features/deposit/deposit_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_starter/l10n/app_localizations.dart';
+import 'package:flutter_starter/shared/currency_converter.dart';
+import 'package:flutter_starter/shared/fee_details.dart';
 import 'package:flutter_starter/shared/grid.dart';
 import 'package:flutter_starter/shared/number_pad.dart';
-import 'package:intl/intl.dart';
 
 class DepositPage extends HookWidget {
   const DepositPage({super.key});
@@ -27,10 +28,14 @@ class DepositPage extends HookWidget {
                   child: Column(
                     children: [
                       buildCurrencyConverter(
-                          context, depositAmount, 'MXN', '17'),
+                          depositAmount,
+                          'MXN',
+                          Loc.of(context).youDeposit,
+                          Loc.of(context).usd,
+                          (1 / 17).toString()),
                       const SizedBox(height: Grid.xl),
                       // these will come from PFI offerings later
-                      buildDepositDetails(context, 'MXN', '17', '0'),
+                      buildFeeDetails(Loc.of(context).usd, 'MXN', '17', '0')
                     ],
                   ),
                 ),
@@ -52,129 +57,26 @@ class DepositPage extends HookWidget {
   }
 
   Widget buildCurrencyConverter(
-      BuildContext context,
       ValueNotifier<String> depositAmount,
       String depositCurrency,
+      String depositLabel,
+      String receiptCurrency,
       String exchangeRate) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Text(
-              NumberFormat.simpleCurrency()
-                  .format(double.parse(depositAmount.value)),
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            const SizedBox(width: Grid.xs),
-            Text(
-              depositCurrency,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-        const SizedBox(height: Grid.xxs),
-        Text(
-          Loc.of(context).youDeposit,
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-        const SizedBox(height: Grid.sm),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Text(
-              NumberFormat.simpleCurrency().format(
-                  double.parse(depositAmount.value) /
-                      double.parse(exchangeRate)),
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            const SizedBox(width: Grid.xs),
-            Baseline(
-              baseline: 0,
-              baselineType: TextBaseline.alphabetic,
-              child: Text(
-                Loc.of(context).usd,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: Grid.xxs),
-        Text(
-          Loc.of(context).youGet,
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-      ],
-    );
+    return CurrencyConverter(
+        originAmount: depositAmount,
+        originCurrency: depositCurrency,
+        originLabel: depositLabel,
+        destinationCurrency: receiptCurrency,
+        exchangeRate: exchangeRate);
   }
 
-  Widget buildDepositDetails(
-    BuildContext context,
-    String depositCurrency,
-    String exchangeRate,
-    String serviceFee,
-  ) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Theme.of(context).colorScheme.outline),
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      padding: const EdgeInsets.all(Grid.xs),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  Loc.of(context).estRate,
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Text(
-                  '1 ${Loc.of(context).usd} = $exchangeRate $depositCurrency',
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                  textAlign: TextAlign.right,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: Grid.sm),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  Loc.of(context).serviceFee,
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Text(
-                  '$serviceFee $depositCurrency',
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                  textAlign: TextAlign.right,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+  Widget buildFeeDetails(String depositCurrency, String receiptCurrency,
+      String exchangeRate, String serviceFee) {
+    return FeeDetails(
+        originCurrency: depositCurrency,
+        destinationCurrency: receiptCurrency,
+        exchangeRate: exchangeRate,
+        serviceFee: serviceFee);
   }
 
   Widget buildNumberPad(ValueNotifier<String> depositAmount) {
