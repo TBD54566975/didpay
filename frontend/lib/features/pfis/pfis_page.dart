c9f3f46600dcc01e06a4e58bf247439a45e94dc4
Wes <wesbillman@users.noreply.github.com>
2024-01-30 11:41:55 -0700

    feat: refactor PFIs and add config

--- frontend/lib/features/pfis/pfis_page.dart
+++ frontend/lib/features/pfis/pfis_page.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/features/pfis/pfi_providers.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_starter/features/pfis/pfi_verification_page.dart';
+import 'package:flutter_starter/features/pfis/pfis_notifier.dart';
 import 'package:flutter_starter/l10n/app_localizations.dart';
+import 'package:flutter_starter/shared/grid.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PfisPage extends HookConsumerWidget {
@@ -9,33 +13,54 @@ class PfisPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Watch the provider and get the AsyncValue object    
+    useEffect(() {
+      Future.delayed(Duration.zero, () {
+        ref.read(pfisProvider.notifier).reload();
+      });
+      return null;
+    }, []);
 
     return Scaffold(
       appBar: AppBar(title: Text(Loc.of(context).selectYourRegion)),
       body: ref.watch(pfisProvider).when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('Error: $error')),
-        data: (pfis) {
-          // Build the list when data is available
-          return ListView(
-            children: pfis.map(
-              (pfi) => ListTile(
-                title: Text(pfi.name),
-                subtitle: Text(pfi.didUri),
-                trailing: const Icon(Icons.chevron_right),
-                onTap: () async {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => PfiVerificationPage(pfi: pfi),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, _) => _buildError(context, ref, error),
+            data: (pfis) => ListView(
+              children: pfis
+                  .map(
+                    (pfi) => ListTile(
+                      title: Text(pfi.name),
+                      subtitle: Text(pfi.didUri),
+                      trailing: const Icon(Icons.chevron_right),
+                      onTap: () async {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => PfiVerificationPage(pfi: pfi),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ).toList(),
-          );
-        },
-      ),
+                  )
+                  .toList(),
+            ),
+          ),
     );
+  }
+
+  Widget _buildError(BuildContext context, WidgetRef ref, Object error) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(error.toString()),
+        const SizedBox(height: Grid.xxs),
+        FilledButton(
+          onPressed: () {
+            ref.read(pfisProvider.notifier).reload();
+          },
+          child: const Text('Reload'),
+        )
+      ],
+    ));
   }
 }
