fe3a9b80ce895a9ca0c0d9cd1ef5717ccb9307d8
Mic Neale <michael.neale@gmail.com>
2024-01-30 16:02:35 +1100

    make it cleaner

--- frontend/lib/features/forms/SchemaForms.dart
+++ frontend/lib/features/forms/SchemaForms.dart
@@ -1,9 +1,63 @@
 import 'package:flutter/material.dart';
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'dart:convert';
+
+
+// example schema: 
+    String jsonSchema = r'''
+{
+  "$schema": "http://json-schema.org/draft-07/schema",
+  "type": "object",
+  "properties": {
+    "accountNumber": {
+      "type": "string",
+      "description": "Mobile Money account number of the Recipient",
+      "title": "Phone Number",
+      "minLength": 12,
+      "maxLength": 12,
+      "pattern": "^+2332[0-9]{8}$"
+    },
+    "reason": {
+      "title": "Reason for sending",
+      "description": "To abide by the travel rules and financial reporting requirements, the reason for sending money",
+      "type": "string"
+    },
+    "accountHolderName": {
+      "type": "string",
+      "title": "Account Holder Name",
+      "description": "Name of the account holder as it appears on the Mobile Money account",
+      "pattern": "^[A-Za-zs'-]+$",
+      "maxLength": 32
+    }
+  },
+  "required": [
+    "accountNumber",
+    "accountHolderName",
+    "reason"
+  ],
+  "additionalProperties": false
+}    
+    ''';
+
+
+// this is an example of a widget that renders the json schema
+class FormsPage extends StatelessWidget {
+  const FormsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Define the JSON schema string
+    return Scaffold(
+      appBar: AppBar(title: Text('Forms Page')),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: DynamicJsonForm(jsonSchemaString: jsonSchema),
+      ),
+    );
+  }
+}
+
 
 class DynamicJsonForm extends StatelessWidget {
   final String jsonSchemaString;
@@ -65,54 +119,3 @@ class DynamicJsonForm extends StatelessWidget {
   }
 }
 
-// this is an example of a widget that renders the json schema
-class FormsPage extends StatelessWidget {
-  const FormsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Define the JSON schema string
-    String jsonSchema = r'''
-{
-  "$schema": "http://json-schema.org/draft-07/schema",
-  "type": "object",
-  "properties": {
-    "accountNumber": {
-      "type": "string",
-      "description": "Mobile Money account number of the Recipient",
-      "title": "Phone Number",
-      "minLength": 12,
-      "maxLength": 12,
-      "pattern": "^+2332[0-9]{8}$"
-    },
-    "reason": {
-      "title": "Reason for sending",
-      "description": "To abide by the travel rules and financial reporting requirements, the reason for sending money",
-      "type": "string"
-    },
-    "accountHolderName": {
-      "type": "string",
-      "title": "Account Holder Name",
-      "description": "Name of the account holder as it appears on the Mobile Money account",
-      "pattern": "^[A-Za-zs'-]+$",
-      "maxLength": 32
-    }
-  },
-  "required": [
-    "accountNumber",
-    "accountHolderName",
-    "reason"
-  ],
-  "additionalProperties": false
-}    
-    ''';
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Forms Page')),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: DynamicJsonForm(jsonSchemaString: jsonSchema),
-      ),
-    );
-  }
-}
