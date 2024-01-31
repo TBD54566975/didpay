65ed4dec084bd62396a470ddfb18cc26507bc12f
Kirah Sapong <kirah@tbd.email>
2024-01-31 01:53:24 -0800

    update send page with animation and add tests

--- frontend/lib/features/send/send_page.dart
+++ frontend/lib/features/send/send_page.dart
@@ -3,13 +3,16 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_starter/l10n/app_localizations.dart';
 import 'package:flutter_starter/shared/number_pad.dart';
 import 'package:flutter_starter/shared/grid.dart';
+import 'package:flutter_starter/shared/animations/invalid_number_pad_input_animation.dart';
+import 'package:flutter_starter/shared/utils/number_pad_input_validation_util.dart';
 
 class SendPage extends HookWidget {
   const SendPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final sendAmount = useState<String>('\$0');
+    final sendAmount = useState<String>('0');
+    final isValidKeyPress = useState<bool>(true);
 
     return Scaffold(
         appBar: AppBar(),
@@ -20,11 +23,11 @@ class SendPage extends HookWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    sendAmount.value,
-                    style: Theme.of(context).textTheme.displayLarge,
-                    textAlign: TextAlign.center,
-                  ),
+                  SizedBox(
+                      child: InvalidNumberPadInputAnimation(
+                          textValue: '\$${sendAmount.value}',
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          shouldAnimate: !isValidKeyPress.value)),
                   const SizedBox(height: Grid.lg),
                 ],
               ),
@@ -33,22 +36,43 @@ class SendPage extends HookWidget {
               padding: const EdgeInsets.symmetric(vertical: Grid.xs),
               child: NumberPad(
                 onKeyPressed: (key) {
-                  sendAmount.value = (sendAmount.value == '\$0')
-                      ? '\$$key'
-                      : '${sendAmount.value}$key';
+                  isValidKeyPress.value = true;
+                  isValidKeyPress.value =
+                      NumberPadInputValidationUtil.validateKeyPress(
+                          sendAmount.value, key);
+
+                  if (isValidKeyPress.value) {
+                    sendAmount.value = (sendAmount.value == '0')
+                        ? key == '.'
+                            ? key.padLeft(2, '0')
+                            : key
+                        : sendAmount.value + key;
+                  }
                 },
                 onDeletePressed: () {
-                  sendAmount.value = (sendAmount.value.length > 2)
-                      ? sendAmount.value
-                          .substring(0, sendAmount.value.length - 1)
-                      : '\$0';
+                  isValidKeyPress.value = true;
+                  isValidKeyPress.value =
+                      NumberPadInputValidationUtil.validateDeletePress(
+                          sendAmount.value);
+
+                  if (isValidKeyPress.value) {
+                    sendAmount.value = (sendAmount.value.length > 1)
+                        ? sendAmount.value
+                            .substring(0, sendAmount.value.length - 1)
+                        : '0';
+                  }
                 },
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: Grid.side),
               child: FilledButton(
-                onPressed: () {},
+                onPressed: () {
+                  isValidKeyPress.value = true;
+                  if (num.parse(sendAmount.value) < 0.01) {
+                    isValidKeyPress.value = false;
+                  }
+                },
                 child: Text(Loc.of(context).send),
               ),
             ),
