8929143911627c3960cde43e308d87c1066ac89f
Ethan Lee <ethanl@tbd.email>
2024-02-02 09:24:54 -0800

    remove unused import

--- frontend/lib/features/home/transaction_details_page.dart
+++ frontend/lib/features/home/transaction_details_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_starter/l10n/app_localizations.dart';
 import 'package:flutter_starter/shared/grid.dart';
-import 'package:flutter_starter/shared/success_page.dart';
 import 'package:flutter_starter/shared/transaction.dart';
 
 class TransactionDetailsPage extends HookWidget {
