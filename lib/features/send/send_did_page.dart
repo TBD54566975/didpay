ca9ed9db908ae181ade4019d434ed8226726ea1f
Ethan Lee <ethanl@tbd.email>
2024-02-12 19:43:14 -0800

    validate qr code scan using `DidJwk.resolve()`

--- lib/features/send/send_did_page.dart
+++ lib/features/send/send_did_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/theme/grid.dart';
 import 'package:didpay/shared/success_page.dart';
+import 'package:web5_flutter/web5_flutter.dart';
 
 class SendDidPage extends HookWidget {
   final _formKey = GlobalKey<FormState>();
@@ -14,6 +15,7 @@ class SendDidPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final focusNode = useFocusNode();
+    final errorText = useState<String?>(null);
     final controller = useTextEditingController();
 
     return Scaffold(
@@ -27,10 +29,9 @@ class SendDidPage extends HookWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    const SizedBox(height: Grid.xxs),
-                    _buildScanQrTile(context, controller),
-                    const SizedBox(height: Grid.xxs),
-                    _buildForm(context, focusNode, controller),
+                    _buildScanQrTile(context, controller, errorText),
+                    _buildForm(context, controller, focusNode, errorText,
+                        Loc.of(context).invalidDid),
                   ],
                 ),
               ),
@@ -39,7 +40,8 @@ class SendDidPage extends HookWidget {
               padding: const EdgeInsets.symmetric(horizontal: Grid.side),
               child: FilledButton(
                 onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
+                  if ((_formKey.currentState?.validate() ?? false) &&
+                      errorText.value == null) {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -56,8 +58,13 @@ class SendDidPage extends HookWidget {
         ));
   }
 
-  Widget _buildForm(BuildContext context, FocusNode focusNode,
-      TextEditingController controller) {
+  Widget _buildForm(
+    BuildContext context,
+    TextEditingController controller,
+    FocusNode focusNode,
+    ValueNotifier<String?> errorText,
+    String errorMessage,
+  ) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: Grid.side),
       child: Form(
@@ -78,53 +85,75 @@ class SendDidPage extends HookWidget {
               ),
               Expanded(
                 child: TextFormField(
-                    focusNode: focusNode,
-                    controller: controller,
-                    onTapOutside: (event) => focusNode.unfocus(),
-                    maxLines: null,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: InputDecoration(
+                  focusNode: focusNode,
+                  controller: controller,
+                  onTap: () => errorText.value = null,
+                  onTapOutside: (event) async {
+                    if (controller.text.isNotEmpty) {
+                      errorText.value = await _isValidDid(controller.text)
+                          ? null
+                          : errorMessage;
+                    }
+                    focusNode.unfocus();
+                  },
+                  maxLines: null,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: InputDecoration(
                       labelText: Loc.of(context).didPrefix,
-                    ),
-                    validator: (value) {
-                      if (value == null ||
-                          !value.startsWith(Loc.of(context).didPrefix)) {
-                        return Loc.of(context).invalidDid;
-                      }
-                      return null;
-                    }),
+                      errorText: errorText.value),
+                  validator: (value) => value == null
+                      ? Loc.of(context).thisFieldCannotBeEmpty
+                      : null,
+                ),
               ),
             ],
           )),
     );
   }
 
-  Widget _buildScanQrTile(
-      BuildContext context, TextEditingController controller) {
-    return ListTile(
-      leading: const Icon(Icons.qr_code),
-      title: Text(
-        Loc.of(context).scanQrCode,
-        style: Theme.of(context).textTheme.bodyMedium,
+  Widget _buildScanQrTile(BuildContext context,
+      TextEditingController controller, ValueNotifier<String?> errorText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: Grid.xxs),
+      child: ListTile(
+        leading: const Icon(Icons.qr_code),
+        title: Text(
+          Loc.of(context).scanQrCode,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: Grid.side),
+        trailing: const Icon(Icons.chevron_right),
+        onTap: () => _scanQrCode(
+            context, controller, errorText, Loc.of(context).invalidDidQrCode),
       ),
-      contentPadding: const EdgeInsets.symmetric(horizontal: Grid.side),
-      trailing: const Icon(Icons.chevron_right),
-      onTap: () => _scanQrCode(context, controller, Loc.of(context).didPrefix),
     );
+  }
+
+  Future<bool> _isValidDid(String did) async {
+    final result = await DidJwk.resolve(did);
+    if (result.hasError()) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   void _scanQrCode(
     BuildContext context,
     TextEditingController controller,
-    String didPrefix,
+    ValueNotifier<String?> errorText,
+    String errorMessage,
   ) async {
-    final scannedDid = await Navigator.of(context).push<String>(
+    // TODO: add isSimulatorProvider to check if running on simulator
+    // TODO: if in simulator, populate the text field with a sample DID (maybe add snack bar saying simulating qr scan)
+
+    final qrValue = await Navigator.of(context).push<String>(
       MaterialPageRoute(builder: (context) => const ScanQrPage()),
     );
-    if (scannedDid != null) {
-      controller.text = scannedDid.startsWith(didPrefix) ? scannedDid : '';
-      _formKey.currentState?.validate();
-    }
+
+    final isValid = qrValue != null && await _isValidDid(qrValue);
+    controller.text = isValid ? qrValue : '';
+    errorText.value = isValid ? null : errorMessage;
   }
 }
