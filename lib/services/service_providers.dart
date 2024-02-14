50db92e63f4b5d717ef1fbaaa50665ecbe91b15e
Ethan Lee <ethanl@tbd.email>
2024-02-14 12:23:15 -0800

    add provider for `DeviceInfoService`

--- lib/services/service_providers.dart
+++ lib/services/service_providers.dart
@@ -1,3 +1,4 @@
+import 'package:didpay/services/device_info_service.dart';
 import 'package:didpay/services/idv_service.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,3 +11,5 @@ final sharedPreferencesProvider =
     Provider<SharedPreferences>((ref) => throw UnimplementedError());
 
 final idvServiceProvider = Provider((ref) => IdvService());
+
+final deviceInfoServiceProvider = Provider((ref) => DeviceInfoService());
