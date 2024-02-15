5a955971d313440d853e4b473a85fb8cbb96a32e
Ethan Lee <ethanl@tbd.email>
2024-02-15 11:55:59 -0800

    consolidate tests

--- test/shared/pending_page_test.dart
+++ test/shared/pending_page_test.dart
@@ -1,6 +1,5 @@
 import 'package:didpay/shared/pending_page.dart';
 import 'package:flutter/material.dart';
-import 'package:didpay/features/app/app_tabs.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../helpers/widget_helpers.dart';
@@ -15,17 +14,6 @@ void main() {
       );
 
       expect(find.text('Your request is pending...'), findsOneWidget);
-    });
-
-    testWidgets('should show progress indicator', (tester) async {
-      await tester.pumpWidget(
-        WidgetHelpers.testableWidget(
-          child: const PendingPage(
-            text: '',
-          ),
-        ),
-      );
-
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
   });
