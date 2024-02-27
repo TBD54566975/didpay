2242d28e79bb9d2f07dd687e2efde5b8f7675b5f
Ethan Lee <ethanl@tbd.email>
2024-02-27 11:17:41 -0800

    add country emoji support

--- lib/features/onboarding/country_page.dart
+++ lib/features/onboarding/country_page.dart
@@ -1,14 +1,17 @@
+import 'package:didpay/features/onboarding/country.dart';
 import 'package:didpay/features/pfis/pfi_page.dart';
 import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CountryPage extends HookWidget {
+class CountryPage extends HookConsumerWidget {
   const CountryPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final countries = ref.read(countryProvider);
     final country = useState<String?>(null);
 
     return Scaffold(
@@ -23,7 +26,7 @@ class CountryPage extends HookWidget {
               Loc.of(context).selectYourCountry,
             ),
             Expanded(
-              child: _buildList(context, country),
+              child: _buildCountryList(context, country, countries),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: Grid.side),
@@ -74,29 +77,58 @@ class CountryPage extends HookWidget {
     );
   }
 
-  Widget _buildList(BuildContext context, ValueNotifier<String?> country) {
+  Widget _buildCountryList(
+    BuildContext context,
+    ValueNotifier<String?> country,
+    List<Country> countries,
+  ) {
     return ListView(
-      children: ['United States', 'Mexico', 'Kenya']
+      children: countries
           .map(
             (c) => ListTile(
               title: Text(
-                c,
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
+                c.name,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(),
               ),
-              trailing: (country.value == c)
+              leading: Container(
+                width: Grid.md,
+                height: Grid.md,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(Grid.xxs)),
+                child: Center(
+                  child: _buildFlag(context, c.code),
+                ),
+              ),
+              trailing: (country.value == c.name)
                   ? Icon(
                       Icons.check,
                       color: Theme.of(context).colorScheme.primary,
                     )
                   : null,
               onTap: () {
-                country.value = c;
+                country.value = c.name;
               },
             ),
           )
           .toList(),
+    );
+  }
+
+  Widget _buildFlag(BuildContext context, String countryCode) {
+    const asciiOffset = 0x41;
+    const flagOffset = 0x1F1E6;
+
+    final firstChar = countryCode.codeUnitAt(0) - asciiOffset + flagOffset;
+    final secondChar = countryCode.codeUnitAt(1) - asciiOffset + flagOffset;
+
+    String emoji =
+        String.fromCharCode(firstChar) + String.fromCharCode(secondChar);
+
+    return Text(
+      emoji,
+      style: Theme.of(context).textTheme.headlineSmall,
     );
   }
 }
