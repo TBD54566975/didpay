2dbe207997696358b0d79c3aaba96e8a0feab4c2
Ethan Lee <ethanl@tbd.email>
2024-03-14 10:59:20 -0700

    simplify funcs

--- lib/features/currency/currency.dart
+++ lib/features/currency/currency.dart
@@ -14,15 +14,13 @@ class Currency {
   });
 
   static int getDecimalDigits(CurrencyCode? currency) {
-    switch (currency) {
-      case CurrencyCode.btc:
-        return 8;
-      default:
-        return 2;
-    }
+    return currency == CurrencyCode.btc ? 8 : 2;
   }
 
-  static String formatFromString(String amount, {String? currency}) {
+  static String formatFromString(
+    String amount, {
+    CurrencyCode? currency,
+  }) {
     final decimalDigits =
         amount.contains('.') ? amount.split('.')[1].length : 0;
     final parsedAmount = double.tryParse(amount) ?? 0.0;
@@ -33,18 +31,16 @@ class Currency {
     );
   }
 
-  static String formatFromDouble(double amount,
-      {String? currency, int? decimalDigits}) {
-    if (currency == CurrencyCode.btc.toString()) {
-      return NumberFormat.currency(
-        symbol: '',
-        decimalDigits: decimalDigits ?? (amount % 1 == 0 ? 0 : 8),
-      ).format(amount);
-    }
-
+  static String formatFromDouble(
+    double amount, {
+    CurrencyCode? currency,
+    int? decimalDigits,
+  }) {
+    final currencyDecimalDigits = Currency.getDecimalDigits(currency);
     return NumberFormat.currency(
       symbol: '',
-      decimalDigits: decimalDigits ?? (amount % 1 == 0 ? 0 : 2),
+      decimalDigits:
+          decimalDigits ?? (amount % 1 == 0 ? 0 : currencyDecimalDigits),
     ).format(amount);
   }
 }
