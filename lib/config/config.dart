89b4655f85c2db6f124f023c25ddba02d766a7dc
Tim Shamilov <shamilovtim@gmail.com>
2024-03-14 18:20:47 -0400

    add comment for a feature flags namespace

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -11,5 +11,6 @@ class Config {
   static const pfisJsonUrl =
       'https://raw.githubusercontent.com/TBD54566975/pfi-providers-data/main/pfis.json';
 
+  // feature flags
   static bool get hasWalletPicker => false;
 }
