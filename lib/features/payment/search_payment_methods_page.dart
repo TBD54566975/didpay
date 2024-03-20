91bfbcad24a61b1e127b51161f94f3048cf45249
Ethan Lee <ethanl@tbd.email>
2024-03-20 15:38:24 -0700

    remove unused imports

--- lib/features/payment/search_payment_methods_page.dart
+++ lib/features/payment/search_payment_methods_page.dart
@@ -1,7 +1,6 @@
 import 'package:didpay/features/payment/payment_method.dart';
 import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/search_field.dart';
-import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
--- lib/features/payment/search_payment_types_page.dart
+++ lib/features/payment/search_payment_types_page.dart
@@ -1,6 +1,4 @@
-import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/search_field.dart';
-import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
