58f62dbf715da35199b6f9ac37153d4c6b4b4a0e
Ethan Lee <ethanl@tbd.email>
2024-04-17 09:59:08 -0700

    use mocks to test `IdvService`

---
+++ test/features/idv/idv_service_test.dart
@@ -0,0 +1,53 @@
+import 'package:didpay/features/idv/idv_service.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:mocktail/mocktail.dart';
+import 'package:web5/web5.dart';
+
+import '../../helpers/mocks.dart';
+
+void main() {
+  late MockHttpClient mockHttpClient;
+  late IdvService subject;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    subject = IdvService(httpClient: mockHttpClient);
+  });
+
+  group('IdvService', () {
+    test('should get IDV request', () async {
+      // arrange
+      final uri = Uri.parse('http://idv.com');
+      final uri2 = Uri.parse('response_uri');
+      final bearerDid = await DidJwk.create();
+      final jwtClaims = JwtClaims(
+        iss: 'did:example:123',
+        aud: 'clientId',
+        sub: 'did:example:123',
+        exp: 1,
+        iat: 2,
+        misc: {
+          'client_id': 's.ClientID',
+          'nonce': 's.Nonce',
+          'response_uri': 'response_uri',
+        },
+      );
+      final jwt = await Jwt.sign(did: bearerDid, payload: jwtClaims);
+      when(() => mockHttpClient.get(uri))
+          .thenAnswer((_) async => http.Response('request=$jwt', 200));
+      when(() => mockHttpClient.post(uri2, body: any(named: 'body')))
+          .thenAnswer((_) async => http.Response('{"url": "123"}', 200));
+
+      // act
+      final result = await subject.getIdvRequest('http://idv.com', bearerDid);
+
+      // assert
+      expect(result, '123');
+
+      verify(() => mockHttpClient.get(Uri.parse('http://idv.com')));
+    });
+  });
+}
