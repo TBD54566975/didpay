a89ef2f1cc31dd5dd2de3119150977b9b865f0dc
Mic Neale <michael.neale@gmail.com>
2024-04-18 14:53:21 +1000

    try using hermit env

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -11,8 +11,23 @@ HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 brew install hermit
 
+# Execute the command and store the output in a variable
+output=$(./bin/hermit env)
+
+# Process each line of the output
+while IFS= read -r line; do
+  # Check if the line contains an environment variable assignment
+  if [[ $line =~ ^([A-Z_]+)=(.*)$ ]]; then
+    # Extract the variable name and value
+    var_name="${BASH_REMATCH[1]}"
+    var_value="${BASH_REMATCH[2]}"
+    
+    # Set the environment variable
+    export "$var_name"="$var_value"
+  fi
+done <<< "$output"
+
 # activate hermit
-export PATH="$PATH:./bin"
 . ./bin/activate-hermit
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
