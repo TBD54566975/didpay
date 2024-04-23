2adccc72ce385b124d3d159d06798d703596ea23
Ethan Lee <ethanl@tbd.email>
2024-04-23 11:56:40 -0700

    add `countryProvider` to account providers

--- lib/features/account/account_providers.dart
+++ lib/features/account/account_providers.dart
@@ -1,5 +1,7 @@
+import 'package:didpay/features/countries/country.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:web5/web5.dart';
 
 final didProvider = Provider<BearerDid>((ref) => throw UnimplementedError());
 final vcProvider = StateProvider<String?>((ref) => null);
+final countryProvider = StateProvider<Country?>((ref) => null);
