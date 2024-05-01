f946d9e7263f00e61227f3b215e5548bce241ea9
Ethan Lee <ethanl@tbd.email>
2024-04-30 17:36:45 -0700

    add `rfqProvider`

--- lib/features/tbdex/tbdex_providers.dart
+++ lib/features/tbdex/tbdex_providers.dart
@@ -1,3 +1,5 @@
+import 'package:didpay/features/account/account_providers.dart';
+import 'package:didpay/features/tbdex/rfq_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tbdex/tbdex.dart';
 
@@ -11,6 +13,35 @@ final offeringsProvider =
     return offerings;
   } on Exception catch (e) {
     throw Exception('Failed to load offerings: $e');
+  }
+});
+
+final rfqProvider =
+    FutureProvider.family.autoDispose<void, RfqState>((ref, rfqState) async {
+  try {
+    final did = ref.read(didProvider);
+
+    // TODO(ethan-tbd): don't hardcode the DID
+    final rfq = Rfq.create(
+      'did:web:localhost%3A8892:ingress',
+      did.uri,
+      CreateRfqData(
+        offeringId: rfqState.offeringId ?? '',
+        payin: CreateSelectedPayinMethod(
+          amount: rfqState.payinAmount ?? '',
+          kind: rfqState.payinMethod?.kind ?? '',
+        ),
+        payout: CreateSelectedPayoutMethod(
+          kind: rfqState.payoutMethod?.kind ?? '',
+        ),
+        claims: [],
+      ),
+    );
+    await rfq.sign(did);
+
+    await TbdexHttpClient.createExchange(rfq, replyTo: rfq.metadata.from);
+  } on Exception catch (e) {
+    throw Exception('Failed to load RFQ: $e');
   }
 });
 
