44a377ef24e69a3b0b5dcf5083481b0613906268
Ethan Lee <ethanl@tbd.email>
2024-05-10 09:33:39 -0700

    wait for rfq to send, then navigate to `ReviewPaymentPage`

--- lib/features/payment/payment_details.dart
+++ lib/features/payment/payment_details.dart
@@ -3,10 +3,10 @@ import 'package:didpay/features/payment/payment_state.dart';
 import 'package:didpay/features/payment/review_payment_page.dart';
 import 'package:didpay/features/tbdex/rfq_state.dart';
 import 'package:didpay/features/tbdex/tbdex.dart';
-import 'package:didpay/l10n/app_localizations.dart';
+import 'package:didpay/features/tbdex/tbdex_providers.dart';
 import 'package:didpay/shared/json_schema_form.dart';
-import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tbdex/tbdex.dart';
 
@@ -27,44 +27,41 @@ class PaymentDetails {
     final fee = paymentMethod.serviceFee;
     final paymentName = paymentMethod.paymentName;
 
-    return isDisabled
-        ? _buildDisabledButton(context)
-        : Expanded(
-            child: JsonSchemaForm(
-              schema: schema,
-              onSubmit: (formData) =>
-                  // TODO(mistermoe): check requiredClaims and navigate to kcc flow if needed, https://github.com/TBD54566975/didpay/issues/122
-                  Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ReviewPaymentPage(
-                    rfq: Tbdex.createRfq(ref, rfqState),
-                    paymentState: paymentState.copyWith(
-                      serviceFee: fee,
-                      paymentName: paymentName,
-                      formData: formData,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-  }
+    final rfq = Tbdex.createRfq(ref, rfqState);
+    final isLoading = useState(false);
 
-  static Widget _buildDisabledButton(BuildContext context) => Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(child: Container()),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: Grid.side),
-              child: FilledButton(
-                onPressed: null,
-                child: Text(Loc.of(context).next),
-              ),
-            ),
-          ],
-        ),
-      );
+    return Expanded(
+      child: JsonSchemaForm(
+        schema: schema,
+        isDisabled: isDisabled,
+        isLoading: isLoading.value,
+        onSubmit: (formData) {
+          // TODO(mistermoe): check requiredClaims and navigate to kcc flow if needed, https://github.com/TBD54566975/didpay/issues/122
+          isLoading.value = true;
+          ref.read(rfqProvider(rfq).future).then(
+                (_) => Navigator.of(context)
+                    .push(
+                      MaterialPageRoute(
+                        builder: (context) => ReviewPaymentPage(
+                          exchangeId: rfq.metadata.id,
+                          paymentState: paymentState.copyWith(
+                            serviceFee: fee,
+                            paymentName: paymentName,
+                            formData: formData,
+                          ),
+                        ),
+                      ),
+                    )
+                    .then(
+                      (_) => WidgetsBinding.instance
+                          .addPostFrameCallback((_) => isLoading.value = false),
+                    ),
+                onError: (error) => isLoading.value = false,
+              );
+        },
+      ),
+    );
+  }
 }
 
 extension _PaymentMethodOperations on Object? {
