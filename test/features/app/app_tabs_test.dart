725868baef44c338a1ebe877ab674cc2a476c1f7
Ethan Lee <ethanl@tbd.email>
2024-05-13 11:51:16 -0700

    update tests to pass

--- test/features/app/app_tabs_test.dart
+++ test/features/app/app_tabs_test.dart
@@ -2,36 +2,56 @@ import 'package:didpay/features/account/account_page.dart';
 import 'package:didpay/features/app/app_tabs.dart';
 import 'package:didpay/features/home/home_page.dart';
 import 'package:didpay/features/send/send_page.dart';
+import 'package:didpay/features/tbdex/tbdex_providers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../helpers/mocks.dart';
 import '../../helpers/widget_helpers.dart';
 
 void main() {
-  testWidgets('should start on HomePage', (tester) async {
-    await tester.pumpWidget(
-      WidgetHelpers.testableWidget(child: const AppTabs()),
-    );
+  group('AppTabs', () {
+    testWidgets('should start on HomePage', (tester) async {
+      await tester.pumpWidget(
+        WidgetHelpers.testableWidget(
+          child: const AppTabs(),
+          overrides: [
+            transactionsProvider.overrideWith(MockTransactionsNotifier.new),
+          ],
+        ),
+      );
+      await tester.pumpAndSettle();
 
-    expect(find.byType(HomePage), findsOneWidget);
-  });
+      expect(find.byType(HomePage), findsOneWidget);
+    });
 
-  testWidgets('should show SendPage when tapped', (tester) async {
-    await tester.pumpWidget(
-      WidgetHelpers.testableWidget(child: const AppTabs()),
-    );
+    testWidgets('should show SendPage when tapped', (tester) async {
+      await tester.pumpWidget(
+        WidgetHelpers.testableWidget(
+          child: const AppTabs(),
+          overrides: [
+            transactionsProvider.overrideWith(MockTransactionsNotifier.new),
+          ],
+        ),
+      );
 
-    await tester.tap(find.text('Send'));
-    await tester.pumpAndSettle();
-    expect(find.byType(SendPage), findsOneWidget);
-  });
+      await tester.tap(find.text('Send'));
+      await tester.pumpAndSettle();
+      expect(find.byType(SendPage), findsOneWidget);
+    });
 
-  testWidgets('should show AccountPage when tapped', (tester) async {
-    await tester.pumpWidget(
-      WidgetHelpers.testableWidget(child: const AppTabs()),
-    );
+    testWidgets('should show AccountPage when tapped', (tester) async {
+      await tester.pumpWidget(
+        WidgetHelpers.testableWidget(
+          child: const AppTabs(),
+          overrides: [
+            transactionsProvider.overrideWith(MockTransactionsNotifier.new),
+          ],
+        ),
+      );
 
-    await tester.tap(find.text('Account'));
-    await tester.pumpAndSettle();
-    expect(find.byType(AccountPage), findsOneWidget);
+      await tester.tap(find.text('Account'));
+      await tester.pumpAndSettle();
+      expect(find.byType(AccountPage), findsOneWidget);
+    });
   });
 }
--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,7 +1,23 @@
+import 'dart:async';
+
+import 'package:didpay/features/tbdex/transactions_notifier.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
+import 'package:tbdex/tbdex.dart';
 import 'package:web5/web5.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
 class MockBearerDid extends Mock implements BearerDid {}
+
+class MockTransactionsNotifier extends TransactionsAsyncNotifier {
+  MockTransactionsNotifier() : super();
+
+  @override
+  FutureOr<List<Exchange>?> build() {
+    return [];
+  }
+
+  @override
+  Future<void> fetch() async {}
+}
