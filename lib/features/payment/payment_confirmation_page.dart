3a404e596567b8cdee2fb1cac676677febc8154a
Ethan Lee <ethanl@tbd.email>
2024-05-16 20:19:13 -0700

    use new state pages for loading and success

--- lib/features/payment/payment_confirmation_page.dart
+++ lib/features/payment/payment_confirmation_page.dart
@@ -1,6 +1,6 @@
 import 'package:didpay/l10n/app_localizations.dart';
-import 'package:didpay/shared/pending_page.dart';
-import 'package:didpay/shared/success_page.dart';
+import 'package:didpay/shared/loading_state.dart';
+import 'package:didpay/shared/success_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -22,8 +22,8 @@ class PaymentConfirmationPage extends HookWidget {
     return Scaffold(
       body: SafeArea(
         child: response.value == null
-            ? PendingPage(text: Loc.of(context).sendingPayment)
-            : SuccessPage(text: Loc.of(context).yourPaymentWasSent),
+            ? LoadingState(text: Loc.of(context).sendingPayment)
+            : SuccessState(text: Loc.of(context).yourPaymentWasSent),
       ),
     );
   }
