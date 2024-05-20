2ca27ddcde05831027557c80c1d1c4981231bd1c
Ethan Lee <ethanl@tbd.email>
2024-05-20 15:30:09 -0700

    add `My PFIs` tile to `AccountPage`

--- lib/features/account/account_page.dart
+++ lib/features/account/account_page.dart
@@ -1,71 +1,120 @@
 import 'package:didpay/features/account/account_did_page.dart';
 import 'package:didpay/features/account/account_vc_page.dart';
+import 'package:didpay/features/pfis/pfis_page.dart';
 import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 // TODO(ethan-tbd): redesign AccountPage, https://github.com/TBD54566975/didpay/issues/130
-class AccountPage extends StatelessWidget {
+class AccountPage extends HookWidget {
   const AccountPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: _buildAccountNavigation(context),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Grid.side,
+                  vertical: Grid.xs,
+                ),
+                child: Text(
+                  Loc.of(context).account,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    _buildMyPfisTile(context),
+                    _buildMyDidTile(context),
+                    _buildMyVcTile(context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 
-  Widget _buildAccountNavigation(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Grid.side,
-              vertical: Grid.xs,
-            ),
-            child: Text(
-              Loc.of(context).account,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
+  Widget _buildMyPfisTile(BuildContext context) => ListTile(
+        title: Text(
+          Loc.of(context).myPfis,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        trailing: const Icon(Icons.chevron_right),
+        leading: Container(
+          width: Grid.md,
+          height: Grid.md,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.circular(Grid.xxs),
           ),
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  title: Text(
-                    Loc.of(context).myDid,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                  trailing: const Icon(Icons.chevron_right),
-                  leading: const Icon(Icons.text_snippet, size: Grid.md),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const AccountDidPage(),
-                      ),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: Text(
-                    Loc.of(context).myVc,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                  trailing: const Icon(Icons.chevron_right),
-                  leading: const Icon(Icons.person, size: Grid.md),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const AccountVCPage(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
+          child: const Center(
+            child: Icon(Icons.currency_exchange),
           ),
-        ],
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const PfisPage(),
+            ),
+          );
+        },
+      );
+
+  Widget _buildMyDidTile(BuildContext context) => ListTile(
+        title: Text(
+          Loc.of(context).myDid,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        trailing: const Icon(Icons.chevron_right),
+        leading: Container(
+          width: Grid.md,
+          height: Grid.md,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.circular(Grid.xxs),
+          ),
+          child: const Center(
+            child: Icon(Icons.text_snippet),
+          ),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const AccountDidPage(),
+            ),
+          );
+        },
+      );
+
+  Widget _buildMyVcTile(BuildContext context) => ListTile(
+        title: Text(
+          Loc.of(context).myVc,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        trailing: const Icon(Icons.chevron_right),
+        leading: Container(
+          width: Grid.md,
+          height: Grid.md,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.circular(Grid.xxs),
+          ),
+          child: const Center(
+            child: Icon(Icons.person),
+          ),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const AccountVCPage(),
+            ),
+          );
+        },
       );
 }
