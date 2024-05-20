2fa3fa918bff73b0e82b20e54bdcd13ff1650190
Ethan Lee <ethanl@tbd.email>
2024-05-20 15:15:05 -0700

    update localized strings

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -308,4 +308,13 @@ class LocEn extends Loc {
 
   @override
   String get orderConfirmed => 'Order confirmed!';
+
+  @override
+  String get removePfi => 'Remove PFI';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get myPfis => 'My PFIs';
 }
