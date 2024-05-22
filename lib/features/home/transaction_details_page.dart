7a97abf9716dac8851954c7cc33a03ad396a0225
Ethan Lee <ethanl@tbd.email>
2024-05-21 23:10:29 -0700

    watch transaction provider to get order status updates

--- lib/features/home/transaction_details_page.dart
+++ lib/features/home/transaction_details_page.dart
@@ -1,55 +1,57 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:didpay/features/home/transaction.dart';
+import 'package:didpay/features/pfis/pfi.dart';
+import 'package:didpay/features/tbdex/transaction_notifier.dart';
 import 'package:didpay/shared/theme/grid.dart';
 import 'package:didpay/shared/utils/currency_util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class TransactionDetailsPage extends HookWidget {
-  final Transaction txn;
+class TransactionDetailsPage extends HookConsumerWidget {
+  final Pfi pfi;
+  final String exchangeId;
 
   const TransactionDetailsPage({
-    required this.txn,
+    required this.pfi,
+    required this.exchangeId,
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    final payoutAmount = CurrencyUtil.formatFromDouble(
-      txn.payoutAmount,
-      currency: txn.payoutCurrency.toUpperCase(),
-    );
-    final payinAmount = CurrencyUtil.formatFromDouble(
-      txn.payinAmount,
-      currency: txn.payinCurrency.toUpperCase(),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final parameters = TransactionProviderParameters(pfi, exchangeId);
+    final transactionState = ref.watch(transactionProvider(parameters));
 
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildHeader(context),
-            Flexible(child: Container()),
-            _buildAmounts(
-              context,
-              payoutAmount,
-              payinAmount,
-            ),
-            Flexible(
-              flex: 3,
-              child: Container(),
-            ),
-            _buildStatusButton(context),
-          ],
+        child: transactionState.when(
+          data: (transaction) => transaction == null
+              ? Container()
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    _buildTransactionType(context, transaction),
+                    Flexible(child: Container()),
+                    _buildTransactionDetails(context, transaction),
+                    Flexible(flex: 3, child: Container()),
+                    _buildTransactionStatus(
+                      context,
+                      transaction.status,
+                    ),
+                  ],
+                ),
+          loading: Container.new,
+          error: (error, stackTrace) => Center(
+            child: Text(error.toString()),
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildHeader(BuildContext context) => Column(
+  Widget _buildTransactionType(BuildContext context, Transaction txn) => Column(
         children: [
           const SizedBox(height: Grid.xs),
           ExcludeSemantics(
@@ -67,80 +69,92 @@ class TransactionDetailsPage extends HookWidget {
         ],
       );
 
-  Widget _buildAmounts(
-    BuildContext context,
-    String payoutAmount,
-    String payinAmount,
-  ) =>
-      Column(
-        children: [
-          const SizedBox(height: Grid.xxl),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            children: [
-              Flexible(
-                child: AutoSizeText(
-                  txn.type == TransactionType.deposit
-                      ? payoutAmount
-                      : payinAmount,
-                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                  maxLines: 1,
-                ),
-              ),
-              const SizedBox(width: Grid.xs),
-              Text(
-                txn.type == TransactionType.deposit
-                    ? txn.payoutCurrency
-                    : txn.payinCurrency,
-                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+  Widget _buildTransactionDetails(
+      BuildContext context, Transaction transaction) {
+    final payoutAmount = CurrencyUtil.formatFromDouble(
+      transaction.payoutAmount,
+      currency: transaction.payoutCurrency.toUpperCase(),
+    );
+    final payinAmount = CurrencyUtil.formatFromDouble(
+      transaction.payinAmount,
+      currency: transaction.payinCurrency.toUpperCase(),
+    );
+
+    return Column(
+      children: [
+        const SizedBox(height: Grid.xxl),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Flexible(
+              child: AutoSizeText(
+                transaction.type == TransactionType.deposit
+                    ? payoutAmount
+                    : payinAmount,
+                style: Theme.of(context).textTheme.displayLarge?.copyWith(
                       fontWeight: FontWeight.bold,
                     ),
+                maxLines: 1,
               ),
-            ],
-          ),
-          const SizedBox(height: Grid.xxs),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            children: [
-              Flexible(
-                child: AutoSizeText(
-                  txn.type == TransactionType.deposit
-                      ? payinAmount
-                      : payoutAmount,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                  maxLines: 1,
-                ),
-              ),
-              const SizedBox(width: Grid.xs),
-              Text(
-                txn.type == TransactionType.deposit
-                    ? txn.payinCurrency
-                    : txn.payoutCurrency,
+            ),
+            const SizedBox(width: Grid.xs),
+            Text(
+              transaction.type == TransactionType.deposit
+                  ? transaction.payoutCurrency
+                  : transaction.payinCurrency,
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ],
+        ),
+        const SizedBox(height: Grid.xxs),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Flexible(
+              child: AutoSizeText(
+                transaction.type == TransactionType.deposit
+                    ? payinAmount
+                    : payoutAmount,
                 style: Theme.of(context).textTheme.titleMedium?.copyWith(
                       color: Theme.of(context).colorScheme.onSurface,
                     ),
+                maxLines: 1,
               ),
-            ],
-          ),
-          const SizedBox(height: Grid.xxs),
-          Text(
-            DateFormat("MMM dd 'at' hh:mm a").format(txn.createdAt.toLocal()),
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-          ),
-        ],
-      );
+            ),
+            const SizedBox(width: Grid.xs),
+            Text(
+              transaction.type == TransactionType.deposit
+                  ? transaction.payinCurrency
+                  : transaction.payoutCurrency,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+          ],
+        ),
+        const SizedBox(height: Grid.xxs),
+        Text(
+          DateFormat("MMM dd 'at' hh:mm a")
+              .format(transaction.createdAt.toLocal()),
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
+      ],
+    );
+  }
 
-  Widget _buildStatusButton(BuildContext context) => Padding(
+  Widget _buildTransactionStatus(
+    BuildContext context,
+    TransactionStatus status,
+  ) =>
+      Padding(
         padding: const EdgeInsets.symmetric(horizontal: Grid.side),
         child: OutlinedButton(
           onPressed: () => {},
@@ -152,9 +166,9 @@ class TransactionDetailsPage extends HookWidget {
             splashFactory: NoSplash.splashFactory,
           ),
           child: Text(
-            txn.status.toString(),
+            status.toString(),
             style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.tertiary,
+                  color: Transaction.getStatusColor(context, status),
                 ),
           ),
         ),
