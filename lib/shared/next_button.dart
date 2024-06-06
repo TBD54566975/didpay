1f29d8e0c9e7c5efe11ffae625cc4a529f5705be
Ethan Lee <ethanl@tbd.email>
2024-06-06 11:54:49 -0700

    add shared `NextButton`

---
+++ lib/shared/next_button.dart
@@ -0,0 +1,20 @@
+import 'package:didpay/l10n/app_localizations.dart';
+import 'package:didpay/shared/theme/grid.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class NextButton extends HookWidget {
+  final void Function()? onPressed;
+  final String? title;
+
+  const NextButton({required this.onPressed, this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Grid.side),
+        child: FilledButton(
+          onPressed: onPressed,
+          child: Text(title ?? Loc.of(context).next),
+        ),
+      );
+}
