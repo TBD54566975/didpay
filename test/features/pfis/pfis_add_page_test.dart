8e6187142748dab26de54617418b2e9d01f639fd
Ethan Lee <ethanl@tbd.email>
2024-06-10 11:09:50 -0700

    update tests to pass

---
+++ test/features/pfis/pfis_add_page_test.dart
@@ -0,0 +1,32 @@
+import 'package:didpay/features/pfis/pfis_add_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../helpers/widget_helpers.dart';
+
+void main() {
+  group('PfisAddPage', () {
+    Widget pfisAddPageTestWidget() => WidgetHelpers.testableWidget(
+          child: const PfisAddPage(),
+          overrides: [],
+        );
+
+    testWidgets('should show QR Code CTA', (tester) async {
+      await tester.pumpWidget(pfisAddPageTestWidget());
+
+      expect(find.textContaining('Scan their QR code'), findsOneWidget);
+    });
+
+    testWidgets('should show input field', (tester) async {
+      await tester.pumpWidget(pfisAddPageTestWidget());
+
+      expect(find.byType(TextField), findsOneWidget);
+    });
+
+    testWidgets('should show add button', (tester) async {
+      await tester.pumpWidget(pfisAddPageTestWidget());
+
+      expect(find.widgetWithText(FilledButton, 'Add'), findsOneWidget);
+    });
+  });
+}
