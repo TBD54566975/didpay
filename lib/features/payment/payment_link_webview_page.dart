8aee47da20f4d7a328f1aa345ecdae53e9f73df8
Ethan Lee <ethanl@tbd.email>
2024-06-26 11:42:52 -0700

    add param for payment link and on submit

--- lib/features/payment/payment_link_webview_page.dart
+++ lib/features/payment/payment_link_webview_page.dart
@@ -1,7 +1,3 @@
-import 'package:didpay/features/app/app.dart';
-import 'package:didpay/l10n/app_localizations.dart';
-import 'package:didpay/shared/error_message.dart';
-import 'package:didpay/shared/loading_message.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -9,19 +5,21 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PaymentLinkWebviewPage extends HookConsumerWidget {
+  final String paymentLink;
+  final Future<void> Function() onSubmit;
+
   const PaymentLinkWebviewPage({
+    required this.paymentLink,
+    required this.onSubmit,
     super.key,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final paymentLink = useState<AsyncValue<String>>(const AsyncLoading());
-
-    paymentLink.value = const AsyncData('https://square.link/u/Em8f8k8F');
+    final isFinished = useState(false);
 
     final settings = InAppWebViewSettings(
-      isInspectable:
-          kDebugMode, // TODO(mistermoe): only enable for debug builds
+      isInspectable: kDebugMode,
       mediaPlaybackRequiresUserGesture: false,
       allowsInlineMediaPlayback: true,
       iframeAllow: 'camera; microphone',
@@ -35,50 +33,30 @@ class PaymentLinkWebviewPage extends HookConsumerWidget {
           icon: const Icon(Icons.close),
         ),
       ),
-      body: paymentLink.value.when(
-        loading: () => LoadingMessage(message: Loc.of(context).startingIdv),
-        error: (error, stackTrace) => SafeArea(
-          child: ErrorMessage(
-            message: error.toString(),
-            onRetry: () => print('retrying...'),
-          ),
-        ),
-        data: (url) {
-          return InAppWebView(
-            initialSettings: settings,
-            onWebViewCreated: (c) {
-              final fullPath =
-                  Uri.parse(url).replace(scheme: 'https').toString();
+      body: InAppWebView(
+        initialSettings: settings,
+        onWebViewCreated: (c) {
+          final url = Uri.parse(paymentLink);
+          final fullPath = url.replace(scheme: 'https').toString();
 
-              c.loadUrl(urlRequest: URLRequest(url: WebUri(fullPath)));
-            },
-            onLoadStart: (controller, url) {
-              print('WOO LOADINGGG $url');
-              if (url.toString().contains('finish.html')) {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const App(),
-                  ),
-                );
-              }
-            },
-            onCloseWindow: (controller) {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const App(),
-                ),
-              );
-            },
-            onReceivedServerTrustAuthRequest: (controller, challenge) async {
-              return ServerTrustAuthResponse(
-                action: ServerTrustAuthResponseAction.PROCEED,
-              );
-            },
-            onPermissionRequest: (controller, permissionRequest) async {
-              return PermissionResponse(
-                action: PermissionResponseAction.GRANT,
-              );
-            },
+          c.loadUrl(urlRequest: URLRequest(url: WebUri(fullPath)));
+        },
+        onLoadStart: (controller, url) {
+          if (url.toString().contains('finish.html')) {
+            isFinished.value = true;
+          }
+        },
+        onCloseWindow: (controller) async {
+          if (isFinished.value) await onSubmit();
+        },
+        onReceivedServerTrustAuthRequest: (controller, challenge) async {
+          return ServerTrustAuthResponse(
+            action: ServerTrustAuthResponseAction.PROCEED,
+          );
+        },
+        onPermissionRequest: (controller, permissionRequest) async {
+          return PermissionResponse(
+            action: PermissionResponseAction.GRANT,
           );
         },
       ),
