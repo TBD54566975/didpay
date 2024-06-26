a131d7f77c13d6951c95bfa242752aa2806a27b1
Ethan Lee <ethanl@tbd.email>
2024-06-26 14:46:17 -0700

    remove error text

--- lib/features/did/did_form.dart
+++ lib/features/did/did_form.dart
@@ -5,11 +5,10 @@ import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:web5/web5.dart';
 
 class DidForm extends HookConsumerWidget {
   final String buttonTitle;
-  final void Function(String) onSubmit;
+  final Future<void> Function(String) onSubmit;
 
   DidForm({required this.buttonTitle, required this.onSubmit, super.key});
 
@@ -17,11 +16,9 @@ class DidForm extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final errorText = useState<String?>(null);
     final focusNode = useFocusNode();
 
     final textController = useTextEditingController();
-    final errorMessage = Loc.of(context).invalidDid;
 
     return Form(
       key: _formKey,
@@ -38,22 +35,11 @@ class DidForm extends HookConsumerWidget {
                   TextFormField(
                     focusNode: focusNode,
                     controller: textController,
-                    onTap: () => errorText.value = null,
-                    onTapOutside: (_) async => _updateErrorText(
-                      textController.text,
-                      errorMessage,
-                      errorText,
-                    ).then((_) => focusNode.unfocus()),
-                    onFieldSubmitted: (_) async => _updateErrorText(
-                      textController.text,
-                      errorMessage,
-                      errorText,
-                    ),
+                    onTapOutside: (_) => focusNode.unfocus(),
                     enableSuggestions: false,
                     autocorrect: false,
                     decoration: InputDecoration(
                       labelText: Loc.of(context).didHint,
-                      errorText: errorText.value,
                     ),
                     validator: (value) => value == null || value.isEmpty
                         ? Loc.of(context).thisFieldCannotBeEmpty
@@ -63,47 +49,13 @@ class DidForm extends HookConsumerWidget {
               ),
             ),
           ),
-          DidQrTile(
-            didTextController: textController,
-            errorText: errorText,
-          ),
+          DidQrTile(didTextController: textController),
           NextButton(
-            onPressed: () {
-              _updateErrorText(
-                textController.text,
-                errorMessage,
-                errorText,
-              ).then(
-                (_) => errorText.value == null
-                    ? onSubmit(textController.text)
-                    : null,
-              );
-            },
+            onPressed: () async => onSubmit(textController.text),
             title: buttonTitle,
           ),
         ],
       ),
     );
-  }
-
-  Future<void> _updateErrorText(
-    String did,
-    String errorMessage,
-    ValueNotifier<String?> errorText,
-  ) async {
-    try {
-      if (did.isEmpty) {
-        errorText.value = errorMessage;
-        return;
-      }
-
-      final resolutionResult = await DidResolver.resolve(did);
-      if (resolutionResult.hasError()) {
-        errorText.value = errorMessage;
-        return;
-      }
-    } on Exception catch (e) {
-      errorText.value = errorMessage;
-    }
   }
 }
