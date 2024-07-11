b547dc93b9aa38f57e1d0761c098686f0e6306e8
Ethan Lee <ethanl@tbd.email>
2024-07-11 11:34:24 -0700

    add `PaymentAmountState`

---
+++ lib/features/payment/payment_amount_state.dart
@@ -0,0 +1,44 @@
+import 'package:decimal/decimal.dart';
+import 'package:didpay/features/pfis/pfi.dart';
+import 'package:tbdex/tbdex.dart';
+
+class PaymentAmountState {
+  String? payinAmount;
+  String? payoutAmount;
+  String? pfiDid;
+  Offering? selectedOffering;
+  Map<Pfi, List<Offering>>? offeringsMap;
+
+  PaymentAmountState({
+    this.payinAmount,
+    this.payoutAmount,
+    this.pfiDid,
+    this.selectedOffering,
+    this.offeringsMap,
+  });
+
+  Decimal get payinDecimalAmount => Decimal.parse(payinAmount ?? '0');
+  Decimal get payoutDecimalAmount => Decimal.parse(payoutAmount ?? '0');
+
+  String? get payinCurrency => selectedOffering?.data.payin.currencyCode;
+  String? get payoutCurrency => selectedOffering?.data.payout.currencyCode;
+
+  String? get exchangeRate => selectedOffering?.data.payoutUnitsPerPayinUnit;
+  String? get exchangeId => selectedOffering?.metadata.id;
+
+  PaymentAmountState copyWith({
+    String? payinAmount,
+    String? payoutAmount,
+    String? pfiDid,
+    Offering? selectedOffering,
+    Map<Pfi, List<Offering>>? offeringsMap,
+  }) {
+    return PaymentAmountState(
+      payinAmount: payinAmount ?? this.payinAmount,
+      payoutAmount: payoutAmount ?? this.payoutAmount,
+      pfiDid: pfiDid ?? this.pfiDid,
+      selectedOffering: selectedOffering ?? this.selectedOffering,
+      offeringsMap: offeringsMap ?? this.offeringsMap,
+    );
+  }
+}
