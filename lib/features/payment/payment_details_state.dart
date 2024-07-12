5035a745dd321a28f02a4025727fd9d22ad526f7
Ethan Lee <ethanl@tbd.email>
2024-07-11 17:11:37 -0700

    add state fields for dap flow

--- lib/features/payment/payment_details_state.dart
+++ lib/features/payment/payment_details_state.dart
@@ -1,18 +1,23 @@
+import 'package:dap/dap.dart';
 import 'package:didpay/features/payment/payment_method.dart';
 
 class PaymentDetailsState {
   final String? paymentCurrency;
+  final String? paymentName;
   final String? selectedPaymentType;
   final PaymentMethod? selectedPaymentMethod;
   final List<PaymentMethod>? paymentMethods;
+  final List<MoneyAddress>? moneyAddresses;
   final List<String>? credentialsJwt;
   final Map<String, String>? formData;
 
   PaymentDetailsState({
     this.paymentCurrency,
+    this.paymentName,
     this.selectedPaymentType,
     this.selectedPaymentMethod,
     this.paymentMethods,
+    this.moneyAddresses,
     this.credentialsJwt,
     this.formData,
   });
@@ -32,18 +37,22 @@ class PaymentDetailsState {
 
   PaymentDetailsState copyWith({
     String? paymentCurrency,
+    String? paymentName,
     String? selectedPaymentType,
     PaymentMethod? selectedPaymentMethod,
     List<PaymentMethod>? paymentMethods,
+    List<MoneyAddress>? moneyAddresses,
     List<String>? credentialsJwt,
     Map<String, String>? formData,
   }) {
     return PaymentDetailsState(
       paymentCurrency: paymentCurrency ?? this.paymentCurrency,
+      paymentName: paymentName ?? this.paymentName,
       selectedPaymentType: selectedPaymentType ?? this.selectedPaymentType,
       selectedPaymentMethod:
           selectedPaymentMethod ?? this.selectedPaymentMethod,
       paymentMethods: paymentMethods ?? this.paymentMethods,
+      moneyAddresses: moneyAddresses ?? this.moneyAddresses,
       credentialsJwt: credentialsJwt ?? this.credentialsJwt,
       formData: formData ?? this.formData,
     );
