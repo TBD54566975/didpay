2688bc4d7a3803b0dfe9b8c74f1718ff870bcddf
Ethan Lee <ethanl@tbd.email>
2024-07-25 14:46:15 -0700

    use `StateProvider` for did provider

--- lib/features/did/did_provider.dart
+++ lib/features/did/did_provider.dart
@@ -1,4 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:web5/web5.dart';
 
-final didProvider = Provider<BearerDid>((ref) => throw UnimplementedError());
+final didProvider =
+    StateProvider<BearerDid>((ref) => throw UnimplementedError());
