22d9182683a5890fa45ee52334608bcef067604d
Ethan Lee <ethanl@tbd.email>
2024-08-05 19:12:17 -0700

    show `ConfirmationMessage` after completing payment link

--- lib/features/payment/payment_link_webview_page.dart
+++ lib/features/payment/payment_link_webview_page.dart
@@ -1,16 +1,14 @@
-import 'package:didpay/features/payment/payment_order_page.dart';
-import 'package:didpay/features/payment/payment_state.dart';
+import 'package:didpay/l10n/app_localizations.dart';
+import 'package:didpay/shared/confirmation_message.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PaymentLinkWebviewPage extends HookConsumerWidget {
-  final PaymentState paymentState;
   final String paymentLink;
 
   const PaymentLinkWebviewPage({
-    required this.paymentState,
     required this.paymentLink,
     super.key,
   });
@@ -44,8 +42,9 @@ class PaymentLinkWebviewPage extends HookConsumerWidget {
             if (context.mounted) {
               Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(
-                  builder: (context) =>
-                      PaymentOrderPage(paymentState: paymentState),
+                  builder: (context) => ConfirmationMessage(
+                    message: Loc.of(context).orderConfirmed,
+                  ),
                 ),
                 (route) => false,
               );
