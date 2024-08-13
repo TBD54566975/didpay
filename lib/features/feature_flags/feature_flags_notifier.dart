02872c8ef3092d05f0564e44b6f5b72e893ad7ec
Ethan Lee <ethanl@tbd.email>
2024-08-13 14:15:16 -0700

    remove flag overwriting

--- lib/features/feature_flags/feature_flags_notifier.dart
+++ lib/features/feature_flags/feature_flags_notifier.dart
@@ -19,16 +19,8 @@ class FeatureFlagsNotifier extends StateNotifier<List<FeatureFlag>> {
   ) async {
     final List<dynamic> flagsJson = await box.get(storageKey) ?? [];
 
-    final featureFlags =
-        Serializer.deserializeList(flagsJson, FeatureFlag.fromJson)
-            .where((flag) => flag.name != remittance.name)
-            .toList();
-
-    final featureFlagsJson =
-        Serializer.serializeList(featureFlags, (flag) => flag.toJson());
-    await box.put(storageKey, featureFlagsJson);
-
-    return FeatureFlagsNotifier._(box, featureFlags);
+    return FeatureFlagsNotifier._(
+        box, Serializer.deserializeList(flagsJson, FeatureFlag.fromJson));
   }
 
   Future<FeatureFlag> add(FeatureFlag flag) async {
