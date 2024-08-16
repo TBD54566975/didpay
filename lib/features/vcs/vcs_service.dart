fb2aac3ade7519eb0a55c8131baffa4c99381218
Ethan Lee <125412902+ethan-tbd@users.noreply.github.com>
2024-08-16 16:21:37 -0700

    feat: add `VcsService` (#267)

---
+++ lib/features/vcs/vcs_service.dart
@@ -0,0 +1,15 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:web5/web5.dart';
+
+final vcsServiceProvider = Provider((_) => VcsService());
+
+class VcsService {
+  List<String>? getRequiredCredentials(
+    PresentationDefinition presentationDefinition,
+    List<String> vcJwts,
+  ) {
+    final credentials = presentationDefinition.selectCredentials(vcJwts);
+
+    return credentials.isEmpty ? null : credentials;
+  }
+}
