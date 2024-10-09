69be086fa4cc388304ebbd26d470512cdc9f8843
Ethan Lee <ethanl@tbd.email>
2024-10-09 11:39:30 -0700

    add lint ignore to `l10n` files

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -1,5 +1,7 @@
 import 'app_localizations.dart';
 
+// ignore_for_file: type=lint
+
 /// The translations for English (`en`).
 class LocEn extends Loc {
   LocEn([String locale = 'en']) : super(locale);
