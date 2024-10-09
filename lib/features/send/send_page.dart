5a7928462187e8bb307cc5fe92353682dc259f2f
Ethan Lee <ethanl@tbd.email>
2024-10-09 11:36:25 -0700

    remove unused import

--- lib/features/send/send_page.dart
+++ lib/features/send/send_page.dart
@@ -10,7 +10,6 @@ import 'package:didpay/features/transaction/transaction.dart';
 import 'package:didpay/l10n/app_localizations.dart';
 import 'package:didpay/shared/error_message.dart';
 import 'package:didpay/shared/header.dart';
-import 'package:didpay/shared/loading_message.dart';
 import 'package:didpay/shared/theme/grid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
