e75d4f197fcab2dbd3f2caeadc9734d003716662
mohitrajsinha <mohitrajsinha321@gmail.com>
2024-10-13 16:18:41 +0530

    Updated currency_dropdown_test.dart

--- test/features/currency/currency_dropdown_test.dart
+++ test/features/currency/currency_dropdown_test.dart
@@ -9,51 +9,45 @@ import '../../helpers/widget_helpers.dart';
 void main() async {
   await TestData.initializeDids();
 
-  late ValueNotifier<PaymentAmountState?> mockState;
-  const testCurrency = 'USD';
+  final testOffering = TestData.getOfferingsMap(payoutCurrencies: ['EUR', 'USD']);
+  final mockState = ValueNotifier<PaymentAmountState?>(
+    PaymentAmountState(
+      offeringsMap: testOffering,
+      selectedOffering: testOffering.values.first.first,
+    ),
+  );
 
-  setUp(() {
-    mockState = ValueNotifier<PaymentAmountState?>(
-      PaymentAmountState(
-        offeringsMap: TestData.getOfferingsMap(),
+  Widget currencyDropdownTestWidget(String testCurrency) {
+    return WidgetHelpers.testableWidget(
+      child: CurrencyDropdown(
+        paymentCurrency: testCurrency,
+        state: mockState,
       ),
     );
-  });
+  }
 
-  Widget currencyDropdownTestWidget() => WidgetHelpers.testableWidget(
-        child: CurrencyDropdown(
-          paymentCurrency: testCurrency,
-          state: mockState,
-        ),
-      );
-
-  testWidgets('should display the initial currency in CurrencyDropdown',
-      (tester) async {
-    await tester.pumpWidget(currencyDropdownTestWidget());
+  testWidgets('should display the initial currency in CurrencyDropdown', (tester) async {
+    const testCurrency = 'USD';    
+    await tester.pumpWidget(currencyDropdownTestWidget(testCurrency));
 
     expect(find.text(testCurrency), findsOneWidget);
   });
 
-  testWidgets('should open Select Currency modal on tap in CurrencyDropdown',
-      (tester) async {
-    await tester.pumpWidget(currencyDropdownTestWidget());
+  testWidgets('should open Select Currency modal on tap in CurrencyDropdown', (tester) async {
+    const testCurrency = 'USD';    
+    await tester.pumpWidget(currencyDropdownTestWidget(testCurrency));
 
     await tester.tap(find.text(testCurrency));
     await tester.pumpAndSettle();
-    expect(
-      find.text('Select currency'),
-      findsOneWidget,
-    );
+    
+    expect(find.text('Select currency'), findsOneWidget);
   });
 
-  testWidgets(
-      'should update PaymentAmountState when a new offering is selected in CurrencyDropdown',
-      (tester) async {
-    await tester.pumpWidget(currencyDropdownTestWidget());
+  testWidgets('should update PaymentAmountState when a new offering is selected in CurrencyDropdown', (tester) async {
+    const testCurrency = 'EUR';
+    await tester.pumpWidget(currencyDropdownTestWidget(testCurrency));
 
-    await tester.tap(
-      find.text(testCurrency),
-    );
+    await tester.tap(find.text(testCurrency));
     await tester.pumpAndSettle();
 
     const selectedCurrency = 'EUR';
@@ -61,6 +55,6 @@ void main() async {
 
     await tester.pumpAndSettle();
 
-    expect(mockState.value?.filterCurrency, selectedCurrency);
+    expect(mockState.value?.payoutCurrency, selectedCurrency);
   });
 }
