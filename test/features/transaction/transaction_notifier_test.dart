24b4dcd69ea9eb82d4315d1191d5f64c7ef23990
Victor Eronmosele <victoreronmosele@gmail.com>
2024-10-16 06:12:39 +0100

    test: add tests for `TransactionNotifier`

---
+++ test/features/transaction/transaction_notifier_test.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+
+import 'package:didpay/features/did/did_provider.dart';
+import 'package:didpay/features/tbdex/tbdex_service.dart';
+import 'package:didpay/features/transaction/transaction.dart';
+import 'package:didpay/features/transaction/transaction_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../helpers/mocks.dart';
+import '../../helpers/riverpod_helpers.dart';
+import '../../helpers/test_data.dart';
+
+class Listener<T> extends Mock {
+  void call(T? previous, T next);
+}
+
+void main() async {
+  await TestData.initializeDids();
+
+  setUpAll(() {
+    registerFallbackValue(
+      const AsyncData<Transaction?>(null),
+    );
+  });
+
+  group('TransactionNotifier', () {
+    final pfi = TestData.getPfi('did:dht:pfiDid');
+    const exchangeId = 'rfq_01ha835rhefwmagsknrrhvaa0k';
+    final parameters = TransactionProviderParameters(pfi, exchangeId);
+
+    final did = TestData.aliceDid;
+
+    test('should set the state to AsyncValue.data(transaction) on read',
+        () async {
+      final mockTbdexService = MockTbdexService();
+      final exchange = TestData.getExchange();
+
+      when(
+        () => mockTbdexService.getExchange(
+          did,
+          pfi.did,
+          exchangeId,
+        ),
+      ).thenAnswer((_) async => exchange);
+
+      final container = createContainer(
+        overrides: [
+          didProvider.overrideWith(
+            (ref) => did,
+          ),
+          tbdexServiceProvider.overrideWith(
+            (ref) => mockTbdexService,
+          ),
+        ],
+      );
+
+      final listener = Listener<AsyncValue<void>>();
+
+      final transactionProviderListenable = transactionProvider(parameters);
+
+      container.listen(transactionProviderListenable, listener.call);
+
+      await container.read(transactionProviderListenable.future);
+
+      verify(
+        () => listener(
+          const AsyncLoading<Transaction?>(),
+          any(that: isA<AsyncData<Transaction?>>()),
+        ),
+      );
+    });
+
+    test('should set the state to AsyncValue.error when error occurs',
+        () async {
+      final mockTbdexService = MockTbdexService();
+
+      when(
+        () => mockTbdexService.getExchange(
+          did,
+          pfi.did,
+          exchangeId,
+        ),
+      ).thenThrow(Exception('Error fetching exchange'));
+
+      final container = createContainer(
+        overrides: [
+          didProvider.overrideWith(
+            (ref) => did,
+          ),
+          tbdexServiceProvider.overrideWith(
+            (ref) => mockTbdexService,
+          ),
+        ],
+      );
+
+      final listener = Listener<AsyncValue<void>>();
+
+      final transactionProviderListenable = transactionProvider(parameters);
+
+      container.listen(transactionProviderListenable, listener.call);
+
+      unawaited(await container.read(transactionProviderListenable.future));
+
+      verify(
+        () => listener(
+          any(that: isA<AsyncError>()),
+          const AsyncData<Transaction?>(null),
+        ),
+      );
+    });
+  });
+}
